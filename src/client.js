e449ac0eeadc1e8b79724715958dc2bb8f44886d
asd0999 <obelixotic@gmail.com>
2021-03-15 21:14:30 -0400

    contentful api integration and .env file for access tokens

--- src/client.js
+++ src/client.js
@@ -1,8 +1,8 @@
 import * as contenful from "contentful";
 
 const client = contenful.createClient({
-    space: "v2o59scsxq8q",
-    accessToken: "d7Gv-CuOP7awcDTx42pZEaCyQ0hxSIa12ZSs-kYOz2Y",
+    space: process.env.SPACE_ID,
+    accessToken: process.env.ACCESS_TOKEN,
 });
 
 export default client;

d939f60e305b3dbc937f4602250e7ac09ac0bae4
asd0999 <obelixotic@gmail.com>
2021-03-16 02:57:10 -0400

    theme dependent project card border on hover

--- public/env.js
+++ public/env.js
@@ -6,7 +6,7 @@ window.env = {
   "npm_config_browser": "",
   "npm_config_commit_hooks": "true",
   "npm_config_viewer": "man",
-  "npm_package_gitHead": "74776222f3bd4de74930031d398fae662c71531f",
+  "npm_package_gitHead": "c4e35b923a81f8d8c0b943626a33e26f3171812a",
   "npm_package_dependencies_contentful": "^8.2.0",
   "npm_config_also": "",
   "npm_config_sign_git_commit": "",
--- src/components/ProjectListItem.js
+++ src/components/ProjectListItem.js
@@ -1,13 +1,17 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 
+import { ThemeContext } from "./ThemeContext";
+
 function ProjectListItem(props) {
+  const context = useContext(ThemeContext);
+
   const { title, thumbnail, blurb, techStack } = props.project.fields;
   const projectId = props.project.sys.id;
 
   return (
     <Link to={`/projects/${projectId}`} className="link-underline">
-      <div className="project-card">
+      <div className={`${context.theme}-theme-project-card project-card`}>
         <div className="project-img-icon">
           <img src={thumbnail.fields.file.url} />
         </div>{" "}
